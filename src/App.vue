c4cd63d2d70bf63784f08f0eadbb19b5b38e2d20
xiuxiu123456 <609700869@qq.com>
2020-07-01 21:14:42 +0800

    22

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png">00000000
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
